4a466fe926c3c7a5bda97dbb50eca82ab8d64b20
Patrik Karlsson <patrik@trixon.se>
2012-09-16 20:03:58 +0200

    Descriptions

--- src/se/trixon/confqual/Item.java
+++ src/se/trixon/confqual/Item.java
@@ -10,23 +10,23 @@ public class Item {
 
 	static {
 		sItems = new LinkedList<Item>();
-		sItems.add(new Item(R.string.key_mcc_mnc, R.string.value_na, R.raw.na));
-		sItems.add(new Item(R.string.key_locale, R.string.value_na, R.raw.na));
-		sItems.add(new Item(R.string.key_smallest_width, R.string.value_smallestWidth, R.raw.na));
-		sItems.add(new Item(R.string.key_available_width, R.string.value_availableWidth, R.raw.na));
-		sItems.add(new Item(R.string.key_available_height, R.string.value_availableHeight, R.raw.na));
+		sItems.add(new Item(R.string.key_mcc_mnc, R.string.value_na, R.raw.mcc_mnc));
+		sItems.add(new Item(R.string.key_locale, R.string.value_na, R.raw.locale));
+		sItems.add(new Item(R.string.key_smallest_width, R.string.value_smallestWidth, R.raw.smallest_width));
+		sItems.add(new Item(R.string.key_available_width, R.string.value_availableWidth, R.raw.available_width));
+		sItems.add(new Item(R.string.key_available_height, R.string.value_availableHeight, R.raw.available_height));
 		sItems.add(new Item(R.string.key_size, R.string.value_size, R.raw.size));
-		sItems.add(new Item(R.string.key_aspect, R.string.value_aspect, R.raw.na));
-		sItems.add(new Item(R.string.key_orientation, R.string.value_orientation, R.raw.na));
-		sItems.add(new Item(R.string.key_uimode, R.string.value_dockmode, R.raw.na));
-		sItems.add(new Item(R.string.key_nightmode, R.string.value_nightmode, R.raw.na));
-		sItems.add(new Item(R.string.key_dpi, R.string.value_dpi, R.raw.na));
-		sItems.add(new Item(R.string.key_touchscreen, R.string.value_touchscreen, R.raw.na));
-		sItems.add(new Item(R.string.key_keyboard, R.string.value_keyboard, R.raw.na));
-		sItems.add(new Item(R.string.key_textinput, R.string.value_textinput, R.raw.na));
-		sItems.add(new Item(R.string.key_navkey, R.string.value_navkey, R.raw.na));
-		sItems.add(new Item(R.string.key_nontouchnav, R.string.value_nontouchnav, R.raw.na));
-		sItems.add(new Item(R.string.key_api, R.string.value_api, R.raw.na));
+		sItems.add(new Item(R.string.key_aspect, R.string.value_aspect, R.raw.aspect));
+		sItems.add(new Item(R.string.key_orientation, R.string.value_orientation, R.raw.orientation));
+		sItems.add(new Item(R.string.key_uimode, R.string.value_dockmode, R.raw.uimode));
+		sItems.add(new Item(R.string.key_nightmode, R.string.value_nightmode, R.raw.nightmode));
+		sItems.add(new Item(R.string.key_dpi, R.string.value_dpi, R.raw.dpi));
+		sItems.add(new Item(R.string.key_touchscreen, R.string.value_touchscreen, R.raw.touchscreen));
+		sItems.add(new Item(R.string.key_keyboard, R.string.value_keyboard, R.raw.keyboard));
+		sItems.add(new Item(R.string.key_textinput, R.string.value_textinput, R.raw.textinput));
+		sItems.add(new Item(R.string.key_navkey, R.string.value_navkey, R.raw.navkey));
+		sItems.add(new Item(R.string.key_nontouchnav, R.string.value_nontouchnav, R.raw.nontouchnav));
+		sItems.add(new Item(R.string.key_api, R.string.value_api, R.raw.api));
 	}
 
 	public Item() {
